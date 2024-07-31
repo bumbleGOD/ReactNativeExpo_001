118854691d1a5f33c513361bc7cf67d12d8e76a5
bumbleGOD <juanjosetm.22@gmail.com>
2024-07-31 08:44:29 -0500

    acabé los templates(vistas)

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import RegisterComponent from './src/components/auth/RegisterComponent'
 import LoginComponent from './src/components/auth/LoginComponent'
+import CVComponent from './src/components/logged/CVComponent'
 
 export class App extends Component {
   render() {
     return (
-      <RegisterComponent></RegisterComponent>
+      // <RegisterComponent></RegisterComponent>
       // <LoginComponent></LoginComponent>
+      <CVComponent></CVComponent>
     )
   }
 }
--- src/components/logged/CVComponent.tsx
+++ src/components/logged/CVComponent.tsx
@@ -1,14 +1,107 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image, StyleSheet, ScrollView } from 'react-native'
+import GlobalStyles from '../../../assets/styles/GlobalStyles'
+import { logoReactIcon } from '../../../assets'
 
 export class CVComponent extends Component {
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
+      <ScrollView>
+        <View style={GlobalStyles.container}>
+          <View style={GlobalStyles.bodyTop}>
+            <Image source={logoReactIcon} style={styles.logo_img} />
+            <Text style={[GlobalStyles.textGeneral, styles.partition]}>Datos de Usuario</Text>
+            <View style={styles.content}>
+              <View style={styles.left}>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>Nombre:</Text>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>Edad:</Text>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>Teléfono:</Text>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>Email:</Text>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>Dirección:</Text>
+              </View>
+              <View style={styles.right}>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>Juan Trujillo</Text>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>17</Text>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>3118977356</Text>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>juanjosetm.22@gmail.com</Text>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>Calle 69bis #105g-10</Text>
+              </View>
+            </View>
+            <Text style={[GlobalStyles.textGeneral, styles.partition]}>Perfil Laboral</Text>
+            <Text style={styles.content}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolor accusantium fugit. Aut, eligendi, consequuntur aspernatur incidunt, nisi sint fugiat minima aperiam velit ea minus animi placeat laborum earum dolorem.</Text>
+            <Text style={[GlobalStyles.textGeneral, styles.partition]}>Skills</Text>
+            <View style={styles.content}>
+              <View style={styles.left}>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>Lenguaje:</Text>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>Html</Text>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>Css3</Text>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>JavaScript</Text>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>Php</Text>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>Python</Text>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>MySql</Text>
+              </View>
+              <View style={styles.right}>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>Años:</Text>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>2</Text>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>2</Text>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>2</Text>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>2</Text>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>4 meses</Text>
+                <Text style={[GlobalStyles.textGeneral, styles.textGeneral]}>2</Text>
+              </View>
+            </View>
+            <Text style={[GlobalStyles.textGeneral, styles.partition]}>Otros</Text>
+            <Text style={[styles.textGeneral, styles.labelLink]}>Sigue mi portafolio para más información</Text>
+            <Text style={GlobalStyles.anchorLink}>Juan.portafolio.web</Text>
+          </View>
+          <View style={GlobalStyles.bodyBottom}>
+
+          </View>
+        </View>
+      </ScrollView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  logo_img: {
+    width: 120,
+    height: 120,
+    resizeMode: 'contain',
+    marginBottom: 40,
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '85%',
+    paddingVertical: 10,
+    textAlign: 'center'
+  },
+  partition: {
+    backgroundColor: '#3FA2F6',
+    color: 'white',
+    padding: 4,
+    width: '85%',
+    textAlign: 'center'
+  },
+  left: {
+    width: '50%',
+    display: 'flex',
+    alignItems: 'center',
+    gap: 10,
+  },
+  right: {
+    width: '50%',
+    display: 'flex',
+    alignItems: 'center',
+    gap: 10,
+  },
+  textGeneral: {
+    fontSize: 14,
+  },
+  labelLink: {
+    marginTop: 15,
+  },
+});
 
 export default CVComponent
